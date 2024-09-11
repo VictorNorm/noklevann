fe1f20ce02769632be2a9067eeff0ccc3e3c24ba
VictorNorm <VictorNorm@users.noreply.github.com>
2024-09-11 12:02:28 +0200

    updated styles for responsive design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,31 +2,35 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer>
-      <table>
-        <tbody>
+    <footer className="footer">
+      <div className="footer__table-wrapper">
+        <table>
+          <tbody>
+            <tr>
+              <td>Henvendelser styret:</td>
+              <td>styret@noklevann.no</td>
+            </tr>
+            <tr>
+              <td>Henvendelser drift:</td>
+              <td>drift@noklevann.no</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="footer__table-wrapper">
+        <table>
+          <tbody>
           <tr>
-            <td>Henvendelser styret:</td>
-            <td>styret@noklevann.no</td>
-          </tr>
-          <tr>
-            <td>Henvendelser drift:</td>
-            <td>drift@noklevann.no</td>
-          </tr>
-        </tbody>
-      </table>
-      <table>
-        <tbody>
-        <tr>
-            <td>Driftleders arbeidstid:</td>
-            <td>Mandag og tirsdag 08:00-16:00, onsdag 10:00-12:00</td>
-          </tr>
-          <tr>
-            <td>Driftleders telefontid:</td>
-            <td>Mandag - onsdag 10:00-12:00</td>
-          </tr>
-        </tbody>
-      </table>
+              <td>Driftleders arbeidstid:</td>
+              <td>Mandag og tirsdag 08:00-16:00, onsdag 10:00-12:00</td>
+            </tr>
+            <tr>
+              <td>Driftleders telefontid:</td>
+              <td>Mandag - onsdag 10:00-12:00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </footer>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,55 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Nav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   // Define the active style
   const activeStyle = {
     borderBottom: "2px solid white",
     // You can add more styles as needed
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      toggleMenu();
+    }
+  };
+
   return (
     <div className='nav'>
-        <ul>
+      <div className="menu-icon"
+      onClick={toggleMenu} 
+      onKeyDown={handleKeyDown}
+      role="button"
+      tabIndex={0}
+      aria-expanded={isMenuOpen}
+      aria-label="Toggle menu">
+      <div className="bar"/>
+      <div className="bar"/>
+      <div className="bar"/>
+    </div>
+    <ul className={isMenuOpen ? 'show' : ''}>
             <li>
               <NavLink to="/" style={({ isActive }) => 
                 isActive ? activeStyle : undefined
-              }>HJEM</NavLink>
+              } onClick={toggleMenu}>HJEM</NavLink>
             </li>
             <li>
               <NavLink to="/bygg" style={({ isActive }) => 
                 isActive ? activeStyle : undefined
-              }>BYGG</NavLink>
+              } onClick={toggleMenu}>BYGG</NavLink>
             </li>
             <li>
               <NavLink to="/uteomraader" style={({ isActive }) => 
                 isActive ? activeStyle : undefined
-              }>UTEOMRÅDER</NavLink>
+              } onClick={toggleMenu}>UTEOMRÅDER</NavLink>
             </li>
             <li>
               <NavLink to="/regler" style={({ isActive }) => 
                 isActive ? activeStyle : undefined
-              }>REGLER</NavLink>
+              } onClick={toggleMenu}>REGLER</NavLink>
             </li>
             <li>
               <NavLink to="/rapporter" style={({ isActive }) => 
                 isActive ? activeStyle : undefined
-              }>RAPPORTER</NavLink>
+              } onClick={toggleMenu}>RAPPORTER</NavLink>
             </li>
             <li>
               <NavLink to="/praktisk" style={({ isActive }) => 
                 isActive ? activeStyle : undefined
-              }>PRAKTISK</NavLink>
+              } onClick={toggleMenu}>PRAKTISK</NavLink>
             </li>
             <li>
               <NavLink to="/nokkelen" style={({ isActive }) => 
                 isActive ? activeStyle : undefined
-              }>NØKKELEN</NavLink>
+              } onClick={toggleMenu}>NØKKELEN</NavLink>
             </li>
             <li>
               <NavLink to="/soknader" style={({ isActive }) => 
                 isActive ? activeStyle : undefined
-              }>SØKNADER</NavLink>
+              } onClick={toggleMenu}>SØKNADER</NavLink>
             </li>
         </ul>
     </div>
