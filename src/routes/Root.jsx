4100907731fa96fbc3f07170a97821023d024392
VictorNorm <VictorNorm@users.noreply.github.com>
2024-09-12 10:48:35 +0200

    updated robots.txt, added sitemap, imported SEO component into root component

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -4,10 +4,12 @@ import Nav from "../components/Nav";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Footer from "../components/Footer";
+import SEO from "../components/SEO";
 
 export default function Root() {
     return (
       <>
+        <SEO />
         <Header />
         <Nav />
         <Search />
